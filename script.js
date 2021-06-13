f7f3a2d3b6d1eec326888fc0c037856cfcd3da02
UroojFatima1 <uroojfatima1518@gmail.com>
2021-06-14 02:11:49 +0500

    comment

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentSlide=0;
+function moveSlide(dir){
+    if (dir==1 && currentSlide==4){currentSlide=-1;}
+    if (dir==-1 && currentSlide==0){currentSlide=5;}
+    currentSlide=currentSlide+dir;
+    const slides=document.querySelectorAll(".slide");
+    console.log(slides);
+    slides.forEach(slide => slide.style.display="none");
+    slides[currentSlide].style.display="flex";}
+s=document.getElementsByClassName("arrow");
+v=document.getElementById("scroll");
+
+
+s.addEventListener("click",dis());
+
+function dis(){v.style.display="flex";
+}
