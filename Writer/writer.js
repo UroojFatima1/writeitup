7b27c89717b7f25748a97dfa8841373efe8185e9
UroojFatima1 <uroojfatima1518@gmail.com>
2021-07-26 23:20:59 +0500

    eriter update

---
+++ Writer/writer.js
@@ -0,0 +1,14 @@
+var image=document.getElementById("fileinput");
+var p=document.getElementById("plustext");
+var btn=document.getElementById("clearb");
+image.addEventListener("change",(event) =>{var img = document.getElementById('output');
+img.src = URL.createObjectURL(event.target.files[0]);
+img.style.display="block";
+p.style.display="none";
+btn.style.display="flex";
+})
+btn.addEventListener("click",(event) =>{var img = document.getElementById('output');
+img.src="";
+img.style.display="none";
+p.style.display="block";
+btn.style.display="none";})
